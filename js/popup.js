5482ff369b7d23370195d94b76e576f912a0ab21
howly <79049845+tharequest@users.noreply.github.com>
2025-11-01 01:14:13 +0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,16 @@
 function openGoogleForm1() {
   window.open(
-    'https://docs.google.com/forms/d/e/1FAIpQLSfcv_u0UlgJgJ_PuKC_ELfI5UUwLufISUdXlNl37VD_NMSAOg/viewform?pli=1',
+    'https://docs.google.com/forms/d/e/1FAIpQLSfcv_u0UlgJgJ_PuKC_ELfI5UUwLufISUdXlNl37VD_NMSAOg/viewform',
     'popupForm',
     'width=700,height=800,scrollbars=yes,resizable=yes'
   );
 }
 function openGoogleForm2() {
   window.open(
-    'https://docs.google.com/forms/d/e/1FAIpQLSeContohURLForm/viewform',
+    'https://docs.google.com/forms/d/e/1FAIpQLSeleQzeEElYtt8PoW8tHm5wL4t6GJbDzfRjhQ6Bwn3KHtSzbg/viewform',
     'popupForm',
     'width=700,height=800,scrollbars=yes,resizable=yes'
   );
 }
+
 
