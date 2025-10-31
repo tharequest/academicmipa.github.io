b56fa974c0251bae29261516c8de769239476cec
howly <79049845+tharequest@users.noreply.github.com>
2025-11-01 01:08:55 +0700

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+function openGoogleForm1() {
+  window.open(
+    'https://docs.google.com/forms/d/e/1FAIpQLSfcv_u0UlgJgJ_PuKC_ELfI5UUwLufISUdXlNl37VD_NMSAOg/viewform?pli=1',
+    'popupForm',
+    'width=700,height=800,scrollbars=yes,resizable=yes'
+  );
+}
+function 2openGoogleForm() {
+  window.open(
+    'https://docs.google.com/forms/d/e/1FAIpQLSeContohURLForm/viewform',
+    'popupForm',
+    'width=700,height=800,scrollbars=yes,resizable=yes'
+  );
+}
